15c40c60b7475fd3ec1a5a0077ae8bb4d17bd06c
Koo <kkm6101@gmail.com>
2026-01-18 16:15:25 +0900

    feat: Add initial HomeController and integrate OpenAPI for API documentation.

--- src/main/java/com/lmsPlatform/lms_platform/controller/HomeController.java
+++ src/main/java/com/lmsPlatform/lms_platform/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.lmsPlatform.lms_platform.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Tag(name = "Home Controller", description = "메인 화면 및 대시보드 관련 컨트롤러")
 @Controller
 public class HomeController {
 
+    @Operation(summary = "메인 대시보드 페이지", description = "LMS 플랫폼의 메인 관리자 대시보드 페이지를 반환합니다.")
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "대시보드 - LMS Platform");
